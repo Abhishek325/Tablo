3a7ea9146585979bd14894cc6a4198c12f02fa27
Abhishek Kumar Singh <iamtheking1abhishek@gmail.com>
2020-05-12 18:59:54 +0530

    Seach integration for table cards

--- FrontEndClient/src/app/main/configure/configure.component.ts
+++ FrontEndClient/src/app/main/configure/configure.component.ts
@@ -11,12 +11,27 @@ export class ConfigureComponent implements OnInit {
 
   tableList: Table[] = [];
   isLoading = true;
+  searchTerm = '';
+  searchResults: Table[] = [];
 
   constructor(private tableService: TableService) { }
+
+  onSearch() {
+    this.tableService.searchTerm = this.searchTerm;
+    if (!this.searchTerm) {
+      this.searchResults = [];
+    } else {
+      this.searchResults = this.tableList.filter(x => x.name.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1);
+    }
+  }
 
   ngOnInit(): void {
     this.tableService.getAllTables().subscribe(res => {
       this.tableList = res['data'];
+      this.searchTerm = this.tableService.searchTerm;
+      if (this.searchTerm) {
+        this.onSearch();
+      }
       this.isLoading = false;
     });
   }
